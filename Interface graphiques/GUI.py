0fcabcbed277bec3e0d155ee4ff120bc78654042
Florian68240 <merckle.florian@orange.fr>
2023-12-08 16:05:02 +0100

    commit message

--- Interface graphiques/GUI.py
+++ Interface graphiques/GUI.py
@@ -1,10 +1,12 @@
 import sys
+import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout
 
 class Fenetre(QWidget):
     def __init__(self):
         super().__init__()
 
+        # Initialiser l'interface utilisateur
         self.initUI()
 
     def initUI(self):
@@ -20,7 +22,7 @@ class Fenetre(QWidget):
         # Connecter le bouton à la fonction close pour quitter l'interface
         self.bouton_quitter.clicked.connect(self.close)
 
-        # Créer la mise en page
+        # Créer la mise en page verticale
         vbox = QVBoxLayout()
         vbox.addWidget(self.label_instruction)
         vbox.addWidget(self.entry)
@@ -28,19 +30,22 @@ class Fenetre(QWidget):
         vbox.addWidget(self.label_message)
         vbox.addWidget(self.bouton_quitter)
 
+        # Appliquer la mise en page à la fenêtre principale
         self.setLayout(vbox)
 
         # Configurer la fenêtre principale
-        self.setGeometry(300, 300, 300, 200)
-        self.setWindowTitle('Interface PyQt')
-        self.show()
+        self.setGeometry(300, 300, 300, 200)  # Définir la position et la taille de la fenêtre
+        self.setWindowTitle('Interface PyQt')  # Définir le titre de la fenêtre
+        self.show()  # Afficher la fenêtre
 
     def afficher_message(self):
-        prenom = self.entry.text()
-        message = "Bonjour " + prenom + "!"
-        self.label_message.setText(message)
+        # Fonction appelée lorsque le bouton "Afficher le message" est cliqué
+        prenom = self.entry.text()  # Obtenir le texte saisi dans le champ d'entrée
+        message = "Bonjour " + prenom + "!"  # Créer un message personnalisé
+        self.label_message.setText(message)  # Afficher le message dans le QLabel
 
 if __name__ == '__main__':
+    # Point d'entrée de l'application
     app = QApplication(sys.argv)
     fenetre = Fenetre()
-    sys.exit(app.exec_())
+    sys.exit(app.exec_())  # Exécuter l'application et attendre la sortie
