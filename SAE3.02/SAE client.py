ac383eb286dd18a088bfcbf87f5c91399f8a7006
Florian68240 <merckle.florian@orange.fr>
2023-12-08 14:07:18 +0100

    commit message

--- SAE3.02/SAE client.py
+++ SAE3.02/SAE client.py
@@ -1,10 +1,48 @@
 import socket
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QMessageBox
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QTextBrowser, QComboBox
+from PyQt5.QtCore import QThread, pyqtSignal
+
+class ClientThread(QThread):
+    message_received = pyqtSignal(str)
+    disconnected = pyqtSignal()
+
+    def __init__(self, host, port, username, password, room):
+        super().__init__()
+        self.host = host
+        self.port = port
+        self.client_socket = None
+        self.username = username
+        self.password = password
+        self.room = room
+
+    def run(self):
+        try:
+            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client_socket.connect((self.host, self.port))
+
+            auth_data = f"{self.username};{self.password};{self.room}"
+            self.client_socket.send(auth_data.encode())
+
+            while True:
+                message = self.client_socket.recv(1024).decode()
+                if not message:
+                    break
+                self.message_received.emit(message)
+        except Exception as e:
+            print(f"Error in ClientThread: {e}")
+        finally:
+            if self.client_socket:
+                self.client_socket.close()
+            self.disconnected.emit()
 
 class ChatClient(QWidget):
-    def __init__(self):
-        super().__init__()
+    message_received = pyqtSignal(str)
 
+    def __init__(self, host, port):
+        super().__init__()
+        self.host = host
+        self.port = port
+        self.client_thread = None
         self.init_ui()
 
     def init_ui(self):
@@ -17,6 +55,14 @@ class ChatClient(QWidget):
         self.entry_password = QLineEdit()
         self.entry_password.setEchoMode(QLineEdit.Password)
 
+        self.label_room = QLabel('Room:')
+        self.combo_box_room = QComboBox()
+        self.combo_box_room.addItem("Général")
+        self.combo_box_room.addItem("Blabla")
+        self.combo_box_room.addItem("Comptabilité")
+        self.combo_box_room.addItem("Informatique")
+        self.combo_box_room.addItem("Marketing")
+
         self.button_authenticate = QPushButton('Authenticate')
         self.button_authenticate.clicked.connect(self.authenticate)
 
@@ -26,45 +72,70 @@ class ChatClient(QWidget):
         self.button_send = QPushButton('Send')
         self.button_send.clicked.connect(self.send_message)
 
+        self.text_browser = QTextBrowser()
+
         layout = QVBoxLayout()
         layout.addWidget(self.label_username)
         layout.addWidget(self.entry_username)
         layout.addWidget(self.label_password)
         layout.addWidget(self.entry_password)
+        layout.addWidget(self.label_room)
+        layout.addWidget(self.combo_box_room)
         layout.addWidget(self.button_authenticate)
         layout.addWidget(self.label_message)
         layout.addWidget(self.entry_message)
         layout.addWidget(self.button_send)
+        layout.addWidget(self.text_browser)
 
         self.setLayout(layout)
 
     def authenticate(self):
-        username = self.entry_username.text()
-        password = self.entry_password.text()
+        try:
+            username = self.entry_username.text()
+            password = self.entry_password.text()
+            room = self.combo_box_room.currentText()
 
-        # Envoyer les données d'authentification au serveur sous forme d'une seule chaîne séparée par un caractère spécial (par exemple, ';')
-        auth_data = f"{username};{password}"
-        client_socket.send(auth_data.encode())
+            self.client_thread = ClientThread(self.host, self.port, username, password, room)
+            self.client_thread.message_received.connect(self.receive_message)
+            self.client_thread.disconnected.connect(self.client_disconnected)
+            self.client_thread.start()
 
-        result = client_socket.recv(1024).decode()
+            # Perform authentication
+            if self.client_thread.client_socket:
+                self.button_authenticate.setEnabled(False)
+                self.button_send.setEnabled(True)
+            else:
+                print("Socket not available.")
 
-        if result == "Authentification successful":
-            QMessageBox.information(self, 'Authentification', 'Authentification réussie!')
-        else:
-            QMessageBox.critical(self, 'Authentification', 'Authentification échouée.')
+        except Exception as e:
+            print(f"Error during authentication: {e}")
 
     def send_message(self):
-        message = self.entry_message.text()
-        client_socket.send(message.encode())
+        if self.client_thread and self.client_thread.client_socket:
+            message = self.entry_message.text()
+            self.client_thread.client_socket.send(message.encode())
+            self.entry_message.clear()
+        else:
+            print("Not connected to the server.")
+
+    def receive_message(self, message):
+        self.message_received.emit(message)
+
+    def client_disconnected(self):
+        self.client_thread.quit()
+        self.client_thread.wait()
+
+        self.button_authenticate.setEnabled(True)
+        self.button_send.setEnabled(False)
 
 if __name__ == '__main__':
     host = '127.0.0.1'
     port = 5557
 
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((host, port))
-
     app = QApplication([])
-    client = ChatClient()
+
+    client = ChatClient(host, port)
+    client.message_received.connect(client.text_browser.append)
     client.show()
+
     app.exec_()
--- SAE3.02/SAE serveur.py
+++ SAE3.02/SAE serveur.py
@@ -1,70 +1,87 @@
 import socket
 import threading
-import mysql.connector
 
-# Connexion à la base de données MySQL
-db = mysql.connector.connect(
-    host="127.0.0.1",
-    user="toto",
-    password="toto",
-    database="SAE302"
-)
-cursor = db.cursor()
+# Dictionnaire pour stocker les informations d'authentification des utilisateurs
+user_credentials = {
+    "Flo": {"Password": "toto", "Room": None},
+    "Mathieu": {"Password": "toto", "Room": None}
+}
 
-# Initialisation de la base de données
-cursor.execute('CREATE TABLE IF NOT EXISTS users (username VARCHAR(255), password VARCHAR(255))')
-cursor.execute('CREATE TABLE IF NOT EXISTS conversations (username VARCHAR(255), message TEXT)')
-db.commit()
+# Liste pour stocker les connexions des clients
+client_connections = []
 
-# Fonction pour gérer la connexion client
-def handle_client(client_socket):
-    # Authentification du client
-    auth_data = client_socket.recv(1024).decode().split(';')
-    username = auth_data[0]
-    password = auth_data[1]
+class ChatServer:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.clients = {}
 
-    # Vérification des identifiants dans la base de données
-    cursor.execute('SELECT * FROM users WHERE username=%s AND password=%s', (username, password))
-    user = cursor.fetchone()
+    def start(self):
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen()
 
-    if user is None:
-        client_socket.send("Authentification failed".encode())
-        client_socket.close()
-        return
+        print(f"Server listening on {self.host}:{self.port}")
 
-    # Authentification réussie
-    client_socket.send("Authentification successful".encode())
+        while True:
+            client_socket, client_address = self.server_socket.accept()
+            threading.Thread(target=self.handle_client, args=(client_socket,)).start()
 
-    while True:
+    def handle_client(self, client_socket):
         try:
-            # Recevoir et enregistrer la conversation
-            message = client_socket.recv(1024).decode()
-            if not message:
-                break  # Si le client se déconnecte, le thread se termine
-            cursor.execute('INSERT INTO conversations VALUES (%s, %s)', (username, message))
-            db.commit()
+            # Receive authentication data
+            auth_data = client_socket.recv(1024).decode()
+            if not auth_data:
+                raise Exception("Invalid authentication data")
+
+            auth_data = auth_data.split(";")
+            if len(auth_data) != 3:
+                raise Exception("Invalid authentication data")
+
+            username, password, room = auth_data
+
+            # Vérification des informations d'authentification
+            if username in user_credentials and user_credentials[username]["Password"] == password:
+                user_credentials[username]["Room"] = room
+            else:
+                raise Exception("Authentication failed")
+
+            self.clients[client_socket] = {"username": username, "room": room}
+
+            print(f"Client connected: {username} (Room: {room})")
+
+            while True:
+                message = client_socket.recv(1024).decode()
+                if not message:
+                    break
+
+                sender_username = self.clients[client_socket]["username"]
+                room = self.clients[client_socket]["room"]
+
+                print(f"Received message from {sender_username} (Room: {room}): {message}")
+
+                for socket_in_room, data_in_room in list(self.clients.items()):
+                    try:
+                        if data_in_room["room"] == room:
+                            socket_in_room.send(f"{sender_username} (Room: {room}): {message}".encode())
+                    except:
+                        # Remove broken connections
+                        del self.clients[socket_in_room]
+
         except Exception as e:
-            print(f"Error: {e}")
-            break
+            print(f"Error handling client: {e}")
+        finally:
+            print("Client disconnected")
+            try:
+                del self.clients[client_socket]
+            except KeyError:
+                pass  # Handle the case where the key does not exist
 
-    client_socket.close()
+            client_socket.close()
 
-# Paramètres du serveur
-host = '127.0.0.1'
-port = 5557
+if __name__ == '__main__':
+    host = '127.0.0.1'
+    port = 5557
 
-# Mise en place du serveur
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
-server.listen(5)
-
-print(f"[*] Server listening on {host}:{port}")
-
-# Attente de connexions clientes
-while True:
-    client, addr = server.accept()
-    print(f"[*] Accepted connection from {addr[0]}:{addr[1]}")
-
-    # Démarrer un thread pour gérer le client
-    client_handler = threading.Thread(target=handle_client, args=(client,))
-    client_handler.start()
+    server = ChatServer(host, port)
+    server.start()
