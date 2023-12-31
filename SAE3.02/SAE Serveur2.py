45e08078aaeedb2f85bbeb9c87a4571154800cde
Florian68240 <merckle.florian@orange.fr>
2023-12-31 15:27:41 +0100

    commit message

--- SAE3.02/SAE Serveur2.py
+++ SAE3.02/SAE Serveur2.py
@@ -30,12 +30,14 @@ class ChatServer:
         self.user_statuses = {}  # Dictionnaire pour stocker l'état de chaque utilisateur
 
     def start(self):
+        # Configuration initiale du serveur
         self.server_socket.bind((self.host, self.port))
         self.server_socket.listen()
 
         print(f"Server listening on {self.host}:{self.port}")
 
         while True:
+            # Attente de nouvelles connexions des clients
             client_socket, client_address = self.server_socket.accept()
             threading.Thread(target=self.handle_client, args=(client_socket,)).start()
 
@@ -43,6 +45,7 @@ class ChatServer:
         username = None
 
         try:
+            # Réception des données d'authentification du client
             auth_data = client_socket.recv(1024).decode()
             if not auth_data:
                 raise Exception("Invalid authentication data")
@@ -51,6 +54,7 @@ class ChatServer:
             command = auth_data[0]
 
             if command == "CREATE_USER":
+                # Traitement de la création d'un nouvel utilisateur
                 new_username, new_password = auth_data[1], auth_data[2]
                 if not self.user_exists_in_database(new_username):
                     self.save_users_from_database(new_username, new_password)
@@ -59,6 +63,7 @@ class ChatServer:
                 else:
                     client_socket.send("USER_EXISTS".encode())
             elif command == "AUTHENTICATE":
+                # Traitement de l'authentification d'un utilisateur existant
                 username, password, room = auth_data[1], auth_data[2], auth_data[3]
                 if self.authenticate_user_in_database(username, password):
                     if username in self.banned_users:
@@ -73,6 +78,7 @@ class ChatServer:
                     client_socket.send("AUTH_FAIL".encode())
                     return
 
+            # Enregistrement du nouveau client dans la liste des clients
             self.clients[client_socket] = {"username": username, "room": room}
             print(f"Client connected: {username} (Room: {room})")
 
@@ -80,33 +86,43 @@ class ChatServer:
             self.update_user_status(username, "connected")
 
             while True:
+                # Gestion des messages reçus du client
                 message = client_socket.recv(1024).decode()
                 if not message:
                     break
 
                 if message.startswith("/kick"):
+                    # Commande pour exclure un utilisateur
                     self.handle_kick_command(message)
                 elif message.startswith("/ban"):
+                    # Commande pour bannir un utilisateur
                     self.handle_ban_command(message)
                 elif message.startswith("/kill"):
+                    # Commande pour arrêter le serveur
                     self.handle_kill_command()
                 elif message.startswith("/room"):
+                    # Commande pour changer la salle d'un utilisateur
                     self.handle_room_command(message)
                 elif message.startswith("/request"):
-                    self.handle_request_command(message)
+                    # Commande pour gérer les demandes d'accès
+                    self.handle_request_command(message, client_socket)
                 elif message.startswith("/verification"):
+                    # Commande pour gérer les vérifications d'accès
                     self.handle_verification_command(message)
                 elif message.startswith("/status"):
-                    # Gérez l'information d'état de l'utilisateur
+                    # Commande pour gérer l'information d'état de l'utilisateur
                     self.handle_status_command(message)
                 else:
+                    # Gestion des messages de discussion réguliers
                     sender_username = self.clients[client_socket]["username"]
                     room = self.clients[client_socket]["room"]
 
                     print(f"Received message from {sender_username} (Room: {room}): {message}")
 
+                    # Sauvegarde du message dans la base de données
                     self.save_message_to_database(sender_username, room, message)
 
+                    # Diffusion du message à tous les clients de la même salle
                     for socket_in_room, data_in_room in list(self.clients.items()):
                         try:
                             if data_in_room["room"] == room:
@@ -117,6 +133,7 @@ class ChatServer:
         except Exception as e:
             print(f"Error handling client: {e}")
         finally:
+            # Gestion de la déconnexion du client
             print("Client disconnected")
             try:
                 del self.clients[client_socket]
@@ -129,21 +146,20 @@ class ChatServer:
             client_socket.close()
 
     def handle_status_command(self, message):
-        # Extrait le nom d'utilisateur et l'état à partir du message (par exemple, si le message est "/status @Mathieu connected", extrayez "Mathieu" et "connected")
+        # Gestion de la commande de statut
         parts = message.split(" ")
         username = parts[1].lstrip("@")
         status = parts[2]
 
-        # Mettez à jour l'état de l'utilisateur dans le dictionnaire user_statuses
+        # Mise à jour de l'état de l'utilisateur
         self.user_statuses[username] = status
 
     def update_user_status(self, username, status):
-        # Mettez à jour l'état de l'utilisateur (par exemple, "connected", "disconnected", "away") dans votre logique.
-        # Vous pouvez stocker ces informations dans un dictionnaire ou une base de données selon vos besoins.
-        # Dans cet exemple, nous utilisons simplement un dictionnaire pour stocker l'état de chaque utilisateur.
+        # Mise à jour de l'état de l'utilisateur
         self.user_statuses[username] = status
 
     def handle_kick_command(self, message):
+        # Gestion de la commande d'exclusion d'un utilisateur
         username_to_kick = message.split(" ")[1].lstrip("@")
 
         if username_to_kick in self.clients.values():
@@ -157,6 +173,7 @@ class ChatServer:
             print(f"User {username_to_kick} not found.")
 
     def handle_ban_command(self, message):
+        # Gestion de la commande de bannissement d'un utilisateur
         username_to_ban = message.split(" ")[1].lstrip("@")
 
         if username_to_ban in self.clients.values():
@@ -171,6 +188,7 @@ class ChatServer:
             print(f"User {username_to_ban} not found.")
 
     def handle_kill_command(self):
+        # Gestion de la commande d'arrêt du serveur
         for client_socket in self.clients.keys():
             try:
                 client_socket.send("Server is shutting down. Goodbye.".encode())
@@ -181,12 +199,11 @@ class ChatServer:
         os._exit(0)
 
     def handle_room_command(self, message):
-        # Extrait le nom d'utilisateur et la salle à partir du message (par exemple, si le message est "/room @Mathieu Informatique", extrayez "Mathieu" et "Informatique")
+        # Gestion de la commande de changement de salle
         parts = message.split(" ")
         username_to_room = parts[1].lstrip("@")
         room = parts[2]
 
-        # Si l'utilisateur est trouvé, changez la salle de l'utilisateur
         if username_to_room in self.clients.values():
             for client_socket, data in list(self.clients.items()):
                 if data["username"] == username_to_room:
@@ -197,39 +214,33 @@ class ChatServer:
             print(f"User {username_to_room} not found.")
 
     def handle_request_command(self, message, client_socket):
-        # Extrait le nom d'utilisateur à partir du message (par exemple, si le message est "/request @Mathieu", extrayez "Mathieu")
+        # Gestion de la commande de demande d'accès
         username_to_request = message.split(" ")[1].lstrip("@")
 
-        # Ajoutez la demande d'accès au dictionnaire access_requests
         self.access_requests[username_to_request] = {"room": None, "client_socket": client_socket}
 
     def handle_verification_command(self, message):
-        # Extrait le nom d'utilisateur et l'action à partir du message (par exemple, si le message est "/verification @Mathieu Allow", extrayez "Mathieu" et "Allow")
+        # Gestion de la commande de vérification d'accès
         parts = message.split(" ")
         username = parts[1].lstrip("@")
         action = parts[2]
 
-        # Gérez la réponse de vérification ici
         if username in self.access_requests:
             if action.lower() == "allow":
-                # Si l'action est "allow", autorisez l'accès à la salle et informez l'utilisateur
                 self.access_requests[username]["client_socket"].send(f"Access to the room granted.".encode())
                 print(f"{username} a obtenu l'accès à la salle.")
             elif action.lower() == "deny":
-                # Si l'action est "deny", refusez l'accès à la salle et informez l'utilisateur
                 self.access_requests[username]["client_socket"].send(f"Access to the room denied.".encode())
                 print(f"{username} s'est vu refuser l'accès à la salle.")
             else:
-                # Si l'action n'est ni "allow" ni "deny", informez l'utilisateur que l'action est invalide
                 self.access_requests[username]["client_socket"].send(f"Invalid verification action.".encode())
                 print(f"Invalid verification action for {username}.")
-            # Supprimez la demande d'accès du dictionnaire access_requests
             del self.access_requests[username]
         else:
-            # Si l'utilisateur n'a pas de demande d'accès correspondante, informez le serveur
             print(f"No access request found for {username}.")
 
     def save_message_to_database(self, username, room, message):
+        # Enregistrement des messages dans la base de données
         cursor = db_connection.cursor()
         query = "INSERT INTO chat_messages (username, room, message, heure_envoi) VALUES (%s, %s, %s, %s)"
         values = (username, room, message, datetime.now())
@@ -237,6 +248,7 @@ class ChatServer:
         db_connection.commit()
 
     def save_users_from_database(self, username, password):
+        # Enregistrement des utilisateurs dans la base de données
         cursor = db_connection.cursor()
         query = "INSERT INTO utilisateur (username, password) VALUES (%s, %s)"
         values = (username, password)
@@ -244,6 +256,7 @@ class ChatServer:
         db_connection.commit()
 
     def user_exists_in_database(self, username):
+        # Vérification de l'existence d'un utilisateur dans la base de données
         cursor = db_connection.cursor()
         query = "SELECT * FROM utilisateur WHERE username = %s"
         values = (username,)
@@ -252,6 +265,7 @@ class ChatServer:
         return result is not None
 
     def authenticate_user_in_database(self, username, password):
+        # Authentification de l'utilisateur par rapport à la base de données
         cursor = db_connection.cursor()
         query = "SELECT * FROM utilisateur WHERE username = %s AND password = %s"
         values = (username, password)
@@ -260,10 +274,8 @@ class ChatServer:
         return result is not None
 
     def verify_room_access(self, username, room):
+        # Vérification d'accès à une salle spécifique
         if room in self.room_verification_required:
-            # Vérifiez ici si l'utilisateur a l'autorisation d'accéder au salon (par exemple, dans la base de données)
-            # Vous pouvez également utiliser une boîte de dialogue pour demander une vérification au client
-            # Pour cet exemple, on suppose que tout le monde a accès à ces salons
             return True
         else:
             return True  # Accès autorisé à tous les autres salons
@@ -273,7 +285,7 @@ if __name__ == '__main__':
     host = '127.0.0.1'
     port = 5558
 
-
+    # Configuration des tables de la base de données
     cursor = db_connection.cursor()
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS chat_messages (
@@ -285,7 +297,6 @@ if __name__ == '__main__':
         )
     """)
 
-
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS utilisateur (
             id INT AUTO_INCREMENT PRIMARY KEY,
@@ -294,7 +305,6 @@ if __name__ == '__main__':
         )
     """)
 
-
-
+    # Démarrage du serveur
     server = ChatServer(host, port)
     server.start()
